17b79e2f8ffd05d881de340a6981d346e685164f
Waruguru Elizabeth <lizkim685@gmail.com>
2022-08-08 10:55:50 +0300

    The Airbnb clone.

---
+++ models/city.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""City Module"""
+
+from models.base_model import BaseModel
+
+class City(BaseModel):
+    """class city
+    Attributes: state_id (str) State ID.
+                name (str) City name. """
+
+    state_id = ""
+    name = ""
